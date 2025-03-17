134c16281b191ea4f8cc3291a4727dc04a3d727c
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-03-17 12:39:05 -0300

    Layout header (WIP)

--- app/ui/layout/admin/admin-layout-header.tsx
+++ app/ui/layout/admin/admin-layout-header.tsx
@@ -1,16 +1,30 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import Image from 'next/image';
+
 export default function AdminLayoutHeader() {
   const title = 'Inicio';
 
   return (
     <div className="absolute w-full p-0 bg-primary text-white h-[56px]">
       <div className="absolute inline-block w-auto ml-2 mt-[7px] p-2 cursor-pointer bg-transparent hover:bg-white/15 rounded-xl">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-          <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
-        </svg>
+        <MenuIcon/>
       </div>
 
       <div className="absolute ml-15 mt-[14px] w-6/12 text-xl">
         PAHO - {title}
+      </div>
+
+      <div className="absolute w-[250px] left-[50%] ml-[-125px] mt-[5px] flex items-center justify-between">
+        <Image
+          src="/flowchart-icon.png"
+          width={136 / 3}
+          height={127 / 3}
+          alt="Flowchar icon"
+        />
+
+        <div className="font-bold text-xl">
+          Algoritmos Clínicos
+        </div>
       </div>
     </div>
   )
